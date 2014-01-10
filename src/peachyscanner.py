e6c38b093071339609770e551381ac876254bc8d
james townley <james@townley.ca>
2014-01-09 22:43:11 -0600

    Working tests

--- src/peachyscanner.py
+++ src/peachyscanner.py
@@ -1,18 +1,25 @@
-import cv
-
+import cv2
 
 class PeachyScanner(object):
 	def run(self):
 		print("Starting Up")
 
 
-
 class Mapper(object):
-	def __init__(self):
-		pass
+	def __init__(self,colour):
+		self.colour = colour
 
-	def get_points(img):
-		pass
+	def _get_first_match(self, ypos, width, img):
+		for xpos in range(0,width):
+			r,g,b = img[xpos,ypos]
+			if ([r,g,b] == self.colour):
+				return xpos
+		return -1
+
+	def get_points(self,img):
+		height = img.shape[0]
+		width = img.shape[1]
+		return [ self._get_first_match(ypos, width, img) for ypos in range(0,height) ] 
 
 if __name__ == "__main__":
 	PeachyScanner().run()
--- test/tests.py
+++ test/tests.py
@@ -3,20 +3,35 @@ import sys, os
 import cv2
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src'))
 
-import peachyscanner
+from peachyscanner import Mapper
 
 class PeachyScannerTest(unittest.TestCase):
 	def test_peachy(self):
 		self.assertEquals(1,1)
 
-class Mapper(unittest.TestCase):
+class MapperTest(unittest.TestCase):
 	def test_given_an_image_point_map_returned(self):
-		img = cv2.imread('TestData/SimpleTestImage1.jpg',1)
-		expected = [0,1,2,3,4,5,6,7,8,9]
+		img = cv2.imread('TestData/SimpleTestImage1.png',1)
+		expected = [i for i in range(0,20)]
 		colour = [255,255,255]
 		mapper = Mapper(colour)
-		actual = mapper.get_points(img) 
+		actual = mapper.get_points(img)
 		self.assertEquals(expected,actual)
 
+	def test_given_an_colour_image_point_map_returned(self):
+		img = cv2.imread('TestData/SimpleTestImage2.png',1)
+		expected = [i for i in range(0,20)]
+		colour = [255,128,0]
+		mapper = Mapper(colour)
+		actual = mapper.get_points(img)
+		self.assertEquals(expected,actual)
+
+	# def test_given_an_variable_colour_image_point_map_returned(self):
+	# 	img = cv2.imread('TestData/SimpleTestImage3.png',1)
+	# 	expected = [i for i in range(0,20)]
+	# 	colour = [255,128,0]
+	# 	mapper = Mapper(colour)
+	# 	actual = mapper.get_points(img)
+	# 	self.assertEquals(expected,actual)
 
 unittest.main()
