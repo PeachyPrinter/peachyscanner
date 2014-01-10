04b4159c55319bb7e08d84d3ca930a3d6e24d62c
james townley <james@townley.ca>
2014-01-09 21:31:56 -0600

    Just starting getting the tests going

---
+++ src/peachyscanner.py
@@ -0,0 +1,18 @@
+import cv
+
+
+class PeachyScanner(object):
+	def run(self):
+		print("Starting Up")
+
+
+
+class Mapper(object):
+	def __init__(self):
+		pass
+
+	def get_points(img):
+		pass
+
+if __name__ == "__main__":
+	PeachyScanner().run()
--- test/tests.py
+++ test/tests.py
@@ -1,5 +1,22 @@
 import unittest
+import sys, os
+import cv2
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src'))
 
-class PeachyScannerTest(unittest.TestCase)
+import peachyscanner
+
+class PeachyScannerTest(unittest.TestCase):
 	def test_peachy(self):
 		self.assertEquals(1,1)
+
+class Mapper(unittest.TestCase):
+	def test_given_an_image_point_map_returned(self):
+		img = cv2.imread('TestData/SimpleTestImage1.jpg',1)
+		expected = [0,1,2,3,4,5,6,7,8,9]
+		colour = [255,255,255]
+		mapper = Mapper(colour)
+		actual = mapper.get_points(img) 
+		self.assertEquals(expected,actual)
+
+
+unittest.main()
