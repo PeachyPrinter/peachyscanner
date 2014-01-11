0d52bbd0488dd8fb1ea666396754668dffbf3714
james townley <james@townley.ca>
2014-01-11 15:41:53 -0600

    Added colour based thresholds

--- src/peachyscanner.py
+++ src/peachyscanner.py
@@ -6,19 +6,39 @@ class PeachyScanner(object):
 
 
 class Mapper(object):
-	def __init__(self,colour):
-		self.colour = colour
+	debug = False
+	def info(self,val):
+		if (self.debug):
+			print(val)
+
+	def __init__(self,colour,threshold):
+		self.b_match,self.g_match, self.r_match = colour
+		self.b_threshold, self.g_threshold, self.r_threshold = threshold
 
 	def _get_first_match(self, ypos, width, img):
 		for xpos in range(0,width):
-			r,g,b = img[xpos,ypos]
-			if ([r,g,b] == self.colour):
-				return xpos
+			b_px = img.item(ypos,xpos,0)
+			g_px = img.item(ypos,xpos,1)
+			r_px = img.item(ypos,xpos,2)
+			self.info('R: %d G: %d B: %d' % (r_px, g_px, b_px))
+			if (
+				abs(self.r_match - r_px) <= self.r_threshold and
+				abs(self.g_match - g_px) <= self.g_threshold and
+				abs(self.b_match - b_px) <= self.b_threshold
+				):
+					return xpos
 		return -1
 
 	def get_points(self,img):
 		height = img.shape[0]
 		width = img.shape[1]
+
+		self.info("Image Height: %d" % height)
+		self.info("Image Width: %d" % width)
+		self.info('MATCH R: %d G: %d B: %d' % (self.r_match,self.g_match, self.b_match))
+		self.info('Threshold R: %d G: %d B: %d' % (self.r_threshold, self.g_threshold, self.b_threshold))
+
+		
 		return [ self._get_first_match(ypos, width, img) for ypos in range(0,height) ] 
 
 if __name__ == "__main__":
--- test/tests.py
+++ test/tests.py
@@ -6,32 +6,73 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(
 from peachyscanner import Mapper
 
 class PeachyScannerTest(unittest.TestCase):
-	def test_peachy(self):
-		self.assertEquals(1,1)
+    def test_peachy(self):
+        self.assertEquals(1,1)
 
 class MapperTest(unittest.TestCase):
-	def test_given_an_image_point_map_returned(self):
-		img = cv2.imread('TestData/SimpleTestImage1.png',1)
-		expected = [i for i in range(0,20)]
-		colour = [255,255,255]
-		mapper = Mapper(colour)
-		actual = mapper.get_points(img)
-		self.assertEquals(expected,actual)
+    def test_given_an_image_point_map_returned(self):
+        img = cv2.imread('TestData/SimpleTestImage1.png',1)
+        expected = [i for i in range(0,20)]
+        colour = [255,255,255]
+        threshold = [0,0,0]
+        mapper = Mapper(colour,threshold)
+        actual = mapper.get_points(img)
+        self.assertEquals(expected,actual)
 
-	def test_given_an_colour_image_point_map_returned(self):
-		img = cv2.imread('TestData/SimpleTestImage2.png',1)
-		expected = [i for i in range(0,20)]
-		colour = [255,128,0]
-		mapper = Mapper(colour)
-		actual = mapper.get_points(img)
-		self.assertEquals(expected,actual)
+    def test_given_an_image_with_non_complete_points_a_point_map_returned(self):
+        img = cv2.imread('TestData/SimpleTestImage4.png',1)
+        expected = [-1,-1] + [i for i in range(2,9)] + [-1,-1] + [i for i in range(11,18)] + [-1,-1]
+        colour = [255,255,255]
+        threshold = [0,0,0]
+        mapper = Mapper(colour,threshold)
+        actual = mapper.get_points(img)
+        self.assertEquals(expected,actual)
 
-	# def test_given_an_variable_colour_image_point_map_returned(self):
-	# 	img = cv2.imread('TestData/SimpleTestImage3.png',1)
-	# 	expected = [i for i in range(0,20)]
-	# 	colour = [255,128,0]
-	# 	mapper = Mapper(colour)
-	# 	actual = mapper.get_points(img)
-	# 	self.assertEquals(expected,actual)
+    def test_given_an_image_with_no_points_a_point_map_returned(self):
+        img = cv2.imread('TestData/SimpleTestImage5.png',1)
+        expected = [-1 for i in range(0,20)]
+        colour = [255,255,255]
+        threshold = [0,0,0]
+        mapper = Mapper(colour,threshold)
+        actual = mapper.get_points(img)
+        self.assertEquals(expected,actual)
+
+    def test_given_an_colour_image_and_specific_colour_a_point_map_returned(self):
+        img = cv2.imread('TestData/SimpleTestImage2.png',1)
+        expected = [i for i in range(0,20)]
+        colour = [255,128,0]
+        threshold = [0,0,0]
+        mapper = Mapper(colour,threshold)
+        actual = mapper.get_points(img)
+        self.assertEquals(expected,actual)
+
+    def test_given_a_threshold_items_in_threshold_work_for_red(self):
+        img = cv2.imread('TestData/RedThresholdTest.png',1)
+        threshold = [0,0,20]
+        expected = [0,0,0,-1,-1] 
+        colour = [128,128,128]
+        mapper = Mapper(colour, threshold)
+        actual = mapper.get_points(img)
+        self.assertEquals(expected,actual)
+
+    def test_given_a_threshold_items_in_threshold_work_for_green(self):
+        img = cv2.imread('TestData/GreenThresholdTest.png',1)
+        threshold = [0,20,0]
+        expected = [0,0,0,-1,-1] 
+        colour = [128,128,128]
+        mapper = Mapper(colour, threshold)
+        actual = mapper.get_points(img)
+        self.assertEquals(expected,actual)
+
+    def test_given_a_threshold_items_in_threshold_work_for_blue(self):
+        img = cv2.imread('TestData/BlueThresholdTest.png',1)
+        threshold = [20,0,0]
+        expected = [0,0,0,-1,-1] 
+        colour = [128,128,128]
+        mapper = Mapper(colour, threshold)
+        actual = mapper.get_points(img)
+        self.assertEquals(expected,actual)
+
+
 
 unittest.main()
