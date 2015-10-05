845ee8058b2e27607fb97237357369db21462eb9
James Townley <software@peachyprinter.com>
2015-10-05 15:32:56 -0600

    Started on save able settings data

--- src/gui.py
+++ src/gui.py
@@ -10,6 +10,7 @@ from api.capture import Capture
 
 
 from ui.camera import CameraControlWrapper
+from ui.posisition import PositionControl
 
 kivy.require('1.9.0')
 
@@ -34,49 +35,6 @@ class CaptureControl(Screen):
     def _disable_all(self):
         for child in self.children:
             child.disabled = True
-
-
-class PositionControl(Screen):
-
-    def select_centre(self):
-        self._disable_all()
-        App.get_running_app().capture.get_centre(self._points_cb)
-
-    def _points_cb(self, pos):
-        self._enable_all()
-        Logger.info('Found centre: {}'.format(pos))
-
-    def select_encoder(self):
-        self._disable_all()
-        App.get_running_app().capture.select_encoder(self._encoder_cb)
-
-    def _encoder_cb(self, encoder_pos):
-        self._enable_all()
-        Logger.info('Found Encoder: {}'.format(encoder_pos))
-
-    def select_roi(self):
-        self._disable_all()
-        App.get_running_app().capture.select_roi(self._roi_cb)
-
-    def _roi_cb(self, roi):
-        self._enable_all()
-        Logger.info('Found ROI (x,y,w,h: {}'.format(roi))
-
-    def encoder_threshold(self, value):
-        App.get_running_app().capture.encoder_threshold = value
-        Logger.info('Encoder Set at : {}'.format(value))
-
-    def encoder_null_zone(self, value):
-        App.get_running_app().capture.encoder_null_zone = value
-        Logger.info('Encoder Null Zone Set at : {}'.format(value))
-
-    def _disable_all(self):
-        for child in self.children:
-            child.disabled = True
-
-    def _enable_all(self):
-        for child in self.children:
-            child.disabled = False
 
 
 class ColorControls(Screen):
