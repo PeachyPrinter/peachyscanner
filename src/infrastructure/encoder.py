750834d85fb5c6f9a5a94f95ed81502925c8e5e0
James Townley <software@peachyprinter.com>
2015-10-15 14:10:54 -0600

    Added history and image mapping to encoder

--- src/infrastructure/encoder.py
+++ src/infrastructure/encoder.py
@@ -10,22 +10,26 @@ class Encoder(object):
                  encoder_sections= None,
                  encoder_point = (0, 0),
                  threshold = 382,
-                 null_zone = 382
+                 null_zone = 382,
+                 history_length = 20
                  ):
         self.threshold = threshold
         self.null_zone = null_zone
         self.encoder_point = tuple(encoder_point)
+        self.ENCODER_COLOR_LOW_BGR =  (0,   0,   255)
+        self.ENCODER_COLOR_HIGH_BGR = (0,   255, 0  )
+        self.ENCODER_COLOR_NULL_BGR = (0,   255, 255)
+        self.THRESHOLD_MARKER_COLOR = (255, 255, 255)
+        self.history_length = history_length
+
         if encoder_sections:
             self._degrees_per_step = 360.0 / encoder_sections
         else:
             self._degrees_per_step = degrees_per_step
+        self._encoder_color_bgr = self.ENCODER_COLOR_LOW_BGR
         self._changes = 0
         self._is_high = False
-        self._ENCODER_COLOR_LOW_BGR =  (0,   0,   255)
-        self._ENCODER_COLOR_HIGH_BGR = (0,   255, 0  )
-        self._ENCODER_COLOR_NULL_BGR = (0,   255, 255)
-        self._THRESHOLD_MARKER_COLOR = (255, 255, 255)
-        self._encoder_color_bgr = self._ENCODER_COLOR_LOW_BGR
+        self._history = []
 
     @property
     def degrees(self):
@@ -34,18 +38,20 @@ class Encoder(object):
     def process(self, image):
         encoder_point = image[self.encoder_point[0]][self.encoder_point[1]]
         encoder_value = np.sum(encoder_point)
+        self._history.append(encoder_value)
+        self._history = self._history[-self.history_length:]
         if encoder_value > self.threshold + self.null_zone:
             if not self._is_high:
                 self._changes += 1
                 self._is_high = True
-                self._encoder_color_bgr = self._ENCODER_COLOR_HIGH_BGR
+                self._encoder_color_bgr = self.ENCODER_COLOR_HIGH_BGR
         elif encoder_value <= self.threshold - self.null_zone:
             if self._is_high:
                 self._changes += 1
                 self._is_high = False
-                self._encoder_color_bgr = self._ENCODER_COLOR_LOW_BGR
+                self._encoder_color_bgr = self.ENCODER_COLOR_LOW_BGR
         else:
-            self._encoder_color_bgr = self._ENCODER_COLOR_NULL_BGR
+            self._encoder_color_bgr = self.ENCODER_COLOR_NULL_BGR
 
     def overlay_encoder(self, image):
         ep = self.encoder_point
@@ -57,18 +63,17 @@ class Encoder(object):
         return image
 
     def overlay_history(self, image):
-        while True:
-            cv2.imshow('im', image)
-            key = chr(cv2.waitKey(1) & 0xFF)
-            if key == 'q':
-                break
+        for idx in range(len(self._history)):
+            height = int((self._history[idx] / (255.0 * 3.0)) * image.shape[0])
+            if self._history[idx] > self.threshold + self.null_zone:
+                color = self.ENCODER_COLOR_HIGH_BGR
+            elif self._history[idx] <= self.threshold - self.null_zone:
+                color = self.ENCODER_COLOR_LOW_BGR
+            else:
+                color = self.ENCODER_COLOR_NULL_BGR
+            image = cv2.line(image, (idx, image.shape[0]),(idx, image.shape[0] - height),color,1)
         theshold_top = int(((self.threshold + self.null_zone) / (255.0 * 3.0)) * image.shape[0])
         theshold_bottom = int(((self.threshold - self.null_zone) / (255.0 * 3.0)) * image.shape[0])
-        image = image = cv2.line(image, (0, theshold_top),(10, theshold_top), self._THRESHOLD_MARKER_COLOR, 1)
-        image = image = cv2.line(image, (0, theshold_bottom),(10, theshold_bottom), self._THRESHOLD_MARKER_COLOR, 1)
-        # while True:
-        #     cv2.imshow('im', image)
-        #     key = chr(cv2.waitKey(1) & 0xFF)
-        #     if key == 'q':
-        #         break
+        image = cv2.line(image, (0, theshold_top),(10, theshold_top), self.THRESHOLD_MARKER_COLOR, 1)
+        image = cv2.line(image, (0, theshold_bottom),(10, theshold_bottom), self.THRESHOLD_MARKER_COLOR, 1)
         return image
--- test/infrastructure_test/test_encoder.py
+++ test/infrastructure_test/test_encoder.py
@@ -10,146 +10,175 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
 from infrastructure.encoder import Encoder
 
 class EncoderTest(unittest.TestCase):
-    def test_degrees_starts_at_0(self):
-        encoder = Encoder()
-        self.assertEqual(0.0, encoder.degrees)
+    # def test_degrees_starts_at_0(self):
+    #     encoder = Encoder()
+    #     self.assertEqual(0.0, encoder.degrees)
 
-    def test_process_given_alternating_BW_adds_expected_degrees(self):
-        blackimage = np.zeros((100,100,3),dtype='uint8')
-        whiteimage = np.ones((100,100,3),dtype='uint8') * 255
-        expected_degrees = 4.0
-        encoder = Encoder()
+    # def test_process_given_alternating_BW_adds_expected_degrees(self):
+    #     blackimage = np.zeros((100,100,3),dtype='uint8')
+    #     whiteimage = np.ones((100,100,3),dtype='uint8') * 255
+    #     expected_degrees = 4.0
+    #     encoder = Encoder()
 
-        for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
+    #     for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
+    #         encoder.process(image)
+
+    #     self.assertEqual(expected_degrees, encoder.degrees)
+
+    # def test_degress_increments_set_amount(self):
+    #     blackimage = np.zeros((100,100,3),dtype='uint8')
+    #     whiteimage = np.ones((100,100,3),dtype='uint8') * 255
+    #     expected_degrees = 8.0
+    #     encoder = Encoder(degrees_per_step=2.0)
+
+    #     for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
+    #         encoder.process(image)
+
+    #     self.assertEqual(expected_degrees, encoder.degrees)
+
+    # def test_encoder_points_increments_set_amount(self):
+    #     blackimage = np.zeros((100,100,3),dtype='uint8')
+    #     whiteimage = np.ones((100,100,3),dtype='uint8') * 255
+    #     expected_degrees = 8.0
+    #     encoder = Encoder(encoder_sections=180)
+
+    #     for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
+    #         encoder.process(image)
+
+    #     self.assertEqual(expected_degrees, encoder.degrees)
+
+
+    # def test_process_given_alternating_BW_adds_expected_degrees_only_on_change(self):
+    #     blackimage = np.zeros((100,100,3),dtype='uint8')
+    #     whiteimage = np.ones((100,100,3),dtype='uint8') * 255
+    #     expected_degrees = 2.0
+    #     encoder = Encoder()
+
+    #     for image in [whiteimage, whiteimage, blackimage, blackimage]:
+    #         encoder.process(image)
+
+    #     self.assertEqual(expected_degrees, encoder.degrees)
+
+
+    # def test_process_given_alternating_BW_adds_expected_degrees_at_specific_point(self):
+    #     blackimage = np.zeros((100,100,3),dtype='uint8')
+    #     whiteimage = np.zeros((100,100,3),dtype='uint8')
+    #     whiteimage[4][4] = [255,255,255]
+    #     expected_degrees = 4.0
+    #     encoder = Encoder(encoder_point=[4, 4])
+
+    #     for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
+    #         encoder.process(image)
+
+    #     self.assertEqual(expected_degrees, encoder.degrees)
+
+    # def test_encoder_point_sets_encoder_point(self):
+    #     blackimage = np.zeros((100,100,3),dtype='uint8')
+    #     whiteimage = np.zeros((100,100,3),dtype='uint8')
+    #     whiteimage[4][4] = [255,255,255]
+    #     expected_degrees = 4.0
+    #     encoder = Encoder(encoder_point=[0, 0])
+    #     encoder.encoder_point = [4, 4]
+
+    #     for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
+    #         encoder.process(image)
+
+    #     self.assertEqual(expected_degrees, encoder.degrees)
+
+    # def test_process_given_alternating_BW_adds_expected_degrees_within_threshold(self):
+    #     blackimage = np.ones((100,100,3),dtype='uint8') * 100
+    #     nullimage = np.ones((100,100,3),dtype='uint8') * 150
+    #     whiteimage = np.ones((100,100,3),dtype='uint8') * 200
+    #     expected_degrees = 4.0
+    #     encoder = Encoder(threshold=450, null_zone=50)
+
+    #     for image in [whiteimage, nullimage, blackimage, nullimage, nullimage,  whiteimage, nullimage, blackimage, ]:
+    #         encoder.process(image)
+
+    #     self.assertEqual(expected_degrees, encoder.degrees)
+
+    # def test_thrshold_changes_threshold(self):
+    #     blackimage = np.ones((100,100,3),dtype='uint8') * 100
+    #     nullimage = np.ones((100,100,3),dtype='uint8') * 150
+    #     whiteimage = np.ones((100,100,3),dtype='uint8') * 200
+    #     expected_degrees = 4.0
+    #     encoder = Encoder(threshold=382, null_zone=382)
+
+    #     encoder.threshold = 450
+    #     encoder.null_zone = 50
+
+    #     for image in [whiteimage, nullimage, blackimage, nullimage, nullimage,  whiteimage, nullimage, blackimage, ]:
+    #         encoder.process(image)
+
+    #     self.assertEqual(expected_degrees, encoder.degrees)
+
+    # def test_overlay_places_encoder_indicator_correct_place(self):
+    #     blackimage = np.zeros((100,100,3),dtype='uint8')
+    #     encoder = Encoder(encoder_point=[50, 50])
+    #     encoder.process(blackimage)
+    #     resulting_image = encoder.overlay_encoder(blackimage)
+    #     #Encode indicator is a circle centered on point with some decoration, just checking for one point
+    #     self.assertTrue((resulting_image[52][52] == [0, 0, 255]).all())
+
+    # def test_overlay_encoder_places_encoder_indicator_correct_place_if_wont_fit(self):
+    #     blackimage = np.zeros((100,100,3),dtype='uint8')
+    #     encoder = Encoder(encoder_point=[2, 2])
+    #     encoder.process(blackimage)
+    #     resulting_image = encoder.overlay_encoder(blackimage)
+    #     #Encode indicator is a circle centered on point with some decoration, just checking for one point
+    #     self.assertTrue((resulting_image[4][4] == [0, 0, 255]).all())
+
+    # def test_overlay_encoder_indicator_color_is_correct(self):
+    #     blackimage = np.zeros((100,100,3),dtype='uint8')
+    #     greyimage = np.ones((100,100,3),dtype='uint8') * 128
+    #     whiteimage = np.ones((100,100,3),dtype='uint8') * 255
+    #     encoder = Encoder(encoder_point=[50, 50],threshold=384, null_zone=50)
+    #     encoder.process(blackimage)
+    #     resulting_image = encoder.overlay_encoder(blackimage)
+    #     self.assertTrue((resulting_image[52][52] == [0, 0, 255]).all())
+    #     encoder.process(greyimage)
+    #     resulting_image = encoder.overlay_encoder(blackimage)
+    #     self.assertTrue((resulting_image[52][52] == [0, 255, 255]).all())
+    #     encoder.process(whiteimage)
+    #     resulting_image = encoder.overlay_encoder(blackimage)
+    #     self.assertTrue((resulting_image[52][52] == [0, 255, 0]).all())
+
+    # def test_overlay_history_shows_threshold_and_null_lines(self):
+    #     image = np.ones((255,255,3),dtype='uint8') * 10
+    #     encoder = Encoder(encoder_point=[50, 50],threshold=300, null_zone=150)
+    #     encoder.process(image)
+    #     resulting_image = encoder.overlay_history(image)
+    #     self.assertTrue((resulting_image[150][9] == [255,255,255]).all())
+    #     self.assertTrue((resulting_image[50][9] == [255,255,255]).all())
+
+    def test_overlays_threshold_history_shows_threshold_and_null_lines(self):
+        blackimage = np.ones((255,255,3),dtype='uint8') * 10
+        greyimage = np.ones((255,255,3),dtype='uint8') * 100
+        whiteimage = np.ones((255,255,3),dtype='uint8') * 200
+        encoder = Encoder(encoder_point=[50, 50],threshold=300, null_zone=150, history_length=10)
+        for image in [
+            blackimage, blackimage, blackimage, blackimage, 
+            greyimage, greyimage, greyimage, greyimage, 
+            whiteimage, whiteimage, whiteimage, whiteimage, 
+            ]:
             encoder.process(image)
-
-        self.assertEqual(expected_degrees, encoder.degrees)
-
-    def test_degress_increments_set_amount(self):
-        blackimage = np.zeros((100,100,3),dtype='uint8')
-        whiteimage = np.ones((100,100,3),dtype='uint8') * 255
-        expected_degrees = 8.0
-        encoder = Encoder(degrees_per_step=2.0)
-
-        for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
-            encoder.process(image)
-
-        self.assertEqual(expected_degrees, encoder.degrees)
-
-    def test_encoder_points_increments_set_amount(self):
-        blackimage = np.zeros((100,100,3),dtype='uint8')
-        whiteimage = np.ones((100,100,3),dtype='uint8') * 255
-        expected_degrees = 8.0
-        encoder = Encoder(encoder_sections=180)
-
-        for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
-            encoder.process(image)
-
-        self.assertEqual(expected_degrees, encoder.degrees)
-
-
-    def test_process_given_alternating_BW_adds_expected_degrees_only_on_change(self):
-        blackimage = np.zeros((100,100,3),dtype='uint8')
-        whiteimage = np.ones((100,100,3),dtype='uint8') * 255
-        expected_degrees = 2.0
-        encoder = Encoder()
-
-        for image in [whiteimage, whiteimage, blackimage, blackimage]:
-            encoder.process(image)
-
-        self.assertEqual(expected_degrees, encoder.degrees)
-
-
-    def test_process_given_alternating_BW_adds_expected_degrees_at_specific_point(self):
-        blackimage = np.zeros((100,100,3),dtype='uint8')
-        whiteimage = np.zeros((100,100,3),dtype='uint8')
-        whiteimage[4][4] = [255,255,255]
-        expected_degrees = 4.0
-        encoder = Encoder(encoder_point=[4, 4])
-
-        for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
-            encoder.process(image)
-
-        self.assertEqual(expected_degrees, encoder.degrees)
-
-    def test_encoder_point_sets_encoder_point(self):
-        blackimage = np.zeros((100,100,3),dtype='uint8')
-        whiteimage = np.zeros((100,100,3),dtype='uint8')
-        whiteimage[4][4] = [255,255,255]
-        expected_degrees = 4.0
-        encoder = Encoder(encoder_point=[0, 0])
-        encoder.encoder_point = [4, 4]
-
-        for image in [whiteimage, blackimage, whiteimage,blackimage, ]:
-            encoder.process(image)
-
-        self.assertEqual(expected_degrees, encoder.degrees)
-
-    def test_process_given_alternating_BW_adds_expected_degrees_within_threshold(self):
-        blackimage = np.ones((100,100,3),dtype='uint8') * 100
-        nullimage = np.ones((100,100,3),dtype='uint8') * 150
-        whiteimage = np.ones((100,100,3),dtype='uint8') * 200
-        expected_degrees = 4.0
-        encoder = Encoder(threshold=450, null_zone=50)
-
-        for image in [whiteimage, nullimage, blackimage, nullimage, nullimage,  whiteimage, nullimage, blackimage, ]:
-            encoder.process(image)
-
-        self.assertEqual(expected_degrees, encoder.degrees)
-
-    def test_thrshold_changes_threshold(self):
-        blackimage = np.ones((100,100,3),dtype='uint8') * 100
-        nullimage = np.ones((100,100,3),dtype='uint8') * 150
-        whiteimage = np.ones((100,100,3),dtype='uint8') * 200
-        expected_degrees = 4.0
-        encoder = Encoder(threshold=382, null_zone=382)
-
-        encoder.threshold = 450
-        encoder.null_zone = 50
-
-        for image in [whiteimage, nullimage, blackimage, nullimage, nullimage,  whiteimage, nullimage, blackimage, ]:
-            encoder.process(image)
-
-        self.assertEqual(expected_degrees, encoder.degrees)
-
-    def test_overlay_places_encoder_indicator_correct_place(self):
-        blackimage = np.zeros((100,100,3),dtype='uint8')
-        encoder = Encoder(encoder_point=[50, 50])
-        encoder.process(blackimage)
-        resulting_image = encoder.overlay_encoder(blackimage)
-        #Encode indicator is a circle centered on point with some decoration, just checking for one point
-        self.assertTrue((resulting_image[52][52] == [0, 0, 255]).all())
-
-    def test_overlay_encoder_places_encoder_indicator_correct_place_if_wont_fit(self):
-        blackimage = np.zeros((100,100,3),dtype='uint8')
-        encoder = Encoder(encoder_point=[2, 2])
-        encoder.process(blackimage)
-        resulting_image = encoder.overlay_encoder(blackimage)
-        #Encode indicator is a circle centered on point with some decoration, just checking for one point
-        self.assertTrue((resulting_image[4][4] == [0, 0, 255]).all())
-
-    def test_overlay_encoder_indicator_color_is_correct(self):
-        blackimage = np.zeros((100,100,3),dtype='uint8')
-        greyimage = np.ones((100,100,3),dtype='uint8') * 128
-        whiteimage = np.ones((100,100,3),dtype='uint8') * 255
-        encoder = Encoder(encoder_point=[50, 50],threshold=384, null_zone=50)
-        encoder.process(blackimage)
-        resulting_image = encoder.overlay_encoder(blackimage)
-        self.assertTrue((resulting_image[52][52] == [0, 0, 255]).all())
-        encoder.process(greyimage)
-        resulting_image = encoder.overlay_encoder(blackimage)
-        self.assertTrue((resulting_image[52][52] == [0, 255, 255]).all())
-        encoder.process(whiteimage)
-        resulting_image = encoder.overlay_encoder(blackimage)
-        self.assertTrue((resulting_image[52][52] == [0, 255, 0]).all())
-
-    def test_overlay_history_shows_threshold_and_null_lines(self):
-        image = np.ones((255,255,3),dtype='uint8') * 10
-        encoder = Encoder(encoder_point=[50, 50],threshold=300, null_zone=150)
-        encoder.process(image)
         resulting_image = encoder.overlay_history(image)
-        self.assertTrue((resulting_image[150][9] == [255,255,255]).all())
-        self.assertTrue((resulting_image[50][9] == [255,255,255]).all())
+        self.assertTrue((resulting_image[255-10][0] == [0,0,255]).all())
+        self.assertFalse((resulting_image[255-11][0] == [0,0,255]).all())
+        self.assertTrue((resulting_image[255-10][1] == [0,0,255]).all())
+        self.assertFalse((resulting_image[255-11][1] == [0,0,255]).all())
+
+        self.assertTrue((resulting_image[255-100][2] == [0,255,255]).all())
+        self.assertTrue((resulting_image[255-100][3] == [0,255,255]).all())
+        self.assertTrue((resulting_image[255-100][4] == [0,255,255]).all())
+        self.assertTrue((resulting_image[255-100][5] == [0,255,255]).all())
+        self.assertFalse((resulting_image[255-101][5] == [0,255,255]).all())
+
+        self.assertTrue((resulting_image[255-200][6] == [0,255,0]).all())
+        self.assertTrue((resulting_image[255-200][7] == [0,255,0]).all())
+        self.assertTrue((resulting_image[255-200][8] == [0,255,0]).all())
+        self.assertTrue((resulting_image[255-200][9] == [0,255,0]).all())
+        self.assertFalse((resulting_image[255-201][9] == [0,255,0]).all())
 
 
 if __name__ == '__main__':
