69837cbdbdbaf2d46b5795e755d6e74822737ebb
james townley <james@townley.ca>
2015-10-22 16:02:48 -0600

    Refining api responsibilities

--- src/api/scanner.py
+++ src/api/scanner.py
@@ -7,21 +7,31 @@ from infrastructure.roi import ROI
 from infrastructure.encoder import Encoder
 
 from camera_control import Camera
+from infrastructure.video_processor import VideoProcessor
+from infrastructure.data_capture import ImageCapture
 
 logger = logging.getLogger('peachy')
 
 
 class ScannerAPI(object):
+
     def __init__(self):
         self.camera = Camera()
-        self.encoder = None
+        self._default_roi = ROI.set_from_points((0,0),(self.camera.shape[0], self.camera.shape[1]),self.camera.shape)
+        self._default_encoder = Encoder((self.camera.shape[0] / 3, self.camera.shape[1] / 2) ,382, 100, 20, 200)
+
+        self.encoder = self._default_encoder
+        self.roi = self._default_roi
+        self.video_processor = VideoProcessor(self.camera, self.encoder, self.roi)
 
     def set_region_of_interest(self, point1, point2):
         frame_shape = self.camera.shape
         self.roi = ROI.set_from_points(point1, point2, [frame_shape[1], frame_shape[0], 3])
+        self.video_processor.roi = self.roi
 
     def capture_image(self, call_back=None):
-        raise Exception('Well Now That Was Easy')
+        self.video_processor.subscribe(ImageCapture(self.encoder.sections))
 
     def configure_encoder(self, point, threshold, null_zone, sections):
         self.encoder = Encoder(point, threshold, null_zone, 20, sections)
+        self.video_processor.encoder = self.encoder
--- test/api_test/test_scanner.py
+++ test/api_test/test_scanner.py
@@ -6,38 +6,37 @@ import logging
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
 
-from mock import patch
+from mock import patch, Mock
 from api.scanner import ScannerAPI
 from infrastructure.roi import ROI
+from infrastructure.data_capture import ImageCapture
 from helpers import TestHelpers
 
 
 class ScannerAPITest(TestHelpers):
-    def test_capture_image_raises_exception_when_roi_not_specified(self):
-        api = ScannerAPI()
-        with self.assertRaises(Exception):
-            api.capture_image()
 
     @patch('api.scanner.Camera')
     def test_set_region_of_interest_should_create_expected_roi(self, mock_camera):
-        api = ScannerAPI()
         cam = mock_camera.return_value
         cam.shape = [300, 100]
+        api = ScannerAPI()
         expected_roi = ROI.set_from_points((0, 0), (151, 90), [100, 300, 3])
         api.set_region_of_interest((0, 0), (151, 90))
         self.assertROIEquals(expected_roi, api.roi)
 
     @patch('api.scanner.Camera')
-    def test_capture_image_raises_exception_when_encoder_not_specified(self, mock_camera):
+    def test_set_region_of_interest_should_replace_the_roi_on_the_video_processor_with_new_roi(self, mock_camera):
         cam = mock_camera.return_value
         cam.shape = [300, 100]
         api = ScannerAPI()
         api.set_region_of_interest((0, 0), (151, 90))
+        self.assertNotEquals(api._default_roi, api.video_processor.roi)
+        self.assertEquals(api.roi, api.video_processor.roi)
 
-        with self.assertRaises(Exception):
-            api.capture_image()
-
-    def test_configure_encoder_should_create_an_encoder_with_the_given_config(self):
+    @patch('api.scanner.Camera')
+    def test_configure_encoder_should_create_an_encoder_with_the_given_config(self, mock_camera):
+        cam = mock_camera.return_value
+        cam.shape = [300, 100]
         api = ScannerAPI()
         api.configure_encoder((50, 50), 500, 100, 200)
         self.assertEquals((50, 50), api.encoder.point)
@@ -45,13 +44,43 @@ class ScannerAPITest(TestHelpers):
         self.assertEquals(100, api.encoder.null_zone)
         self.assertEquals(200, api.encoder.sections)
 
-    def test_configure_encoder_should_replace_the_existing_encoder_with_new_encoder(self):
+    @patch('api.scanner.Camera')
+    def test_configure_encoder_should_replace_the_existing_encoder_with_new_encoder(self, mock_camera):
+        cam = mock_camera.return_value
+        cam.shape = [300, 100]
         api = ScannerAPI()
         api.configure_encoder((50, 50), 500, 100, 200)
         initial = api.encoder
         api.configure_encoder((51, 51), 500, 100, 200)
         self.assertNotEquals(api.encoder, initial)
 
+    @patch('api.scanner.Camera')
+    def test_configure_encoder_should_replace_the_encoder_on_the_video_processor_with_new_encoder(self, mock_camera):
+        cam = mock_camera.return_value
+        cam.shape = [300, 100]
+        api = ScannerAPI()
+        api.configure_encoder((50, 50), 500, 100, 200)
+        self.assertNotEquals(api._default_encoder, api.video_processor.encoder)
+        self.assertEquals(api.encoder, api.video_processor.encoder)
+
+    @patch('api.scanner.Camera')
+    def test_init_should_construct_a_video_processor(self, mock_camera):
+        cam = mock_camera.return_value
+        cam.shape = [300, 100]
+        api = ScannerAPI()
+        self.assertTrue(api.video_processor is not None)
+        self.assertEquals(api._default_encoder, api.video_processor.encoder)
+        self.assertEquals(api._default_roi, api.video_processor.roi)
+
+    @patch('api.scanner.Camera')
+    def test_capture_image_should_create_an_image_handler_and_subscribe_it_to_video_processor(self, mock_camera):
+        cam = mock_camera.return_value
+        cam.shape = [300, 100]
+        api = ScannerAPI()
+        callback = Mock()
+        api.capture_image(callback)
+        self.assertTrue(len(api.video_processor.handlers) > 0)
+        self.assertTrue(hasattr(api.video_processor.handlers[0], 'handle'))
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level='INFO')
