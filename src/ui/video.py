6a66d3f451919ce7dbe9cca57453718e9ba1e136
James Townley <software@peachyprinter.com>
2015-10-29 08:49:05 -0600

    Added toggleable graphic elements

--- src/ui/video.py
+++ src/ui/video.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
-from kivy.properties import ObjectProperty, ListProperty
+from kivy.properties import ObjectProperty, ListProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.logger import Logger
@@ -16,9 +16,14 @@ Builder.load_file('ui/video.kv')
 
 class ImageDisplay(BoxLayout):
     scanner = ObjectProperty()
-    image = ObjectProperty(Texture.create(size=(1, 1), colorfmt='rgb'))
+    texture = ObjectProperty(Texture.create(size=(1, 1), colorfmt='rgb'))
     tex_pos = ListProperty([0, 0])
     tex_size = ListProperty([1, 1])
+    show_encoder = BooleanProperty(True)
+    show_encoder_history = BooleanProperty(True)
+    show_roi = BooleanProperty(True)
+    show_mask = BooleanProperty(True)
+    show_center = BooleanProperty(True)
 
     def __init__(self, **kwargs):
         super(ImageDisplay, self).__init__(**kwargs)
@@ -27,26 +32,49 @@ class ImageDisplay(BoxLayout):
             self.ret = Line(rectangle=(0, 0, 0, 0))
             Color(1, 1, 1)
             self.center_line = Line(points=[0, 0, 0, 0], width=1)
+
+        App.get_running_app().video_pos = self.tex_pos
+        App.get_running_app().video_size = self.tex_size
+
         Clock.schedule_interval(self.update_image, 1 / 30.)
 
-    def update_image(self, largs):
-        image_data = self.scanner.get_feed_image(self.size)
-        image = image_data['roi_frame']
-        overlays = cv2.add(image_data['encoder'],image_data['history'],)
+    def _add_overlays_to_image(self, image, overlays):
         img2gray = cv2.cvtColor(overlays, cv2.COLOR_BGR2GRAY)
         ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
         image = cv2.bitwise_and(image, image, mask=cv2.bitwise_not(mask))
-        image = cv2.add(image, overlays)
+        return cv2.add(image, overlays)
+
+    def _image_to_texture(self, image):
         image = np.rot90(np.swapaxes(image, 0, 1))
         texture = Texture.create(size=(image.shape[1], image.shape[0]), colorfmt='rgb')
         texture.blit_buffer(image.tostring(), colorfmt='bgr', bufferfmt='ubyte')
-        self.tex_size = texture.size
+        return texture
+
+    def on_texture(self, instance, value):
+        self.tex_size = self.texture.size
         self.tex_pos = (self.x + (self.width - self.tex_size[0]) / 2, self.y + (self.height - self.tex_size[1]) / 2)
-        center = self.tex_pos[0] + (self.tex_size[0] / 2)
-        self.center_line.points = [center, self.tex_pos[1], center, self.tex_pos[1] + self.tex_size[1]]
-        self.image = texture
-        App.get_running_app().video_pos = self.tex_pos
-        App.get_running_app().video_size = self.tex_size
+
+        if self.show_center:
+            center = self.tex_pos[0] + (self.tex_size[0] / 2)
+            self.center_line.points = [center, self.tex_pos[1], center, self.tex_pos[1] + self.tex_size[1]]
+        else:
+            self.center_line.points = [0, 0, 0, 0]
+
+    def update_image(self, largs):
+        image_data = self.scanner.get_feed_image(self.size)
+        if self.show_roi:
+            image = image_data['roi_frame']
+        else:
+            image = image_data['frame']
+        overlays = np.zeros(image.shape, dtype=image.dtype)
+        if self.show_encoder:
+            overlays = cv2.add(overlays, image_data['encoder'])
+        if self.show_encoder_history:
+            overlays = cv2.add(overlays, image_data['history'])
+
+        image = self._add_overlays_to_image(image, overlays)
+        self.texture = self._image_to_texture(image)
+
 
     def selection(self, x, y, w, h):
         with self.canvas.after:
