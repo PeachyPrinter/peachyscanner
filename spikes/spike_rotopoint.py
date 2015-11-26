b9eefd689585c31a0751179dc965c5bb6d17b7ef
James Townley <software@peachyprinter.com>
2015-11-26 11:40:11 -0600

    Added faster rotational spike code

--- spikes/spike_rotopoint.py
+++ spikes/spike_rotopoint.py
@@ -18,7 +18,10 @@ class RotoPoint(object):
 
     def rotate_points(self, points_xyz, angle):
         r_mat = self.rotation_matrix(angle)
-        return np.dot(r_mat, points_xyz)
+
+        return np.dot(r_mat, points_xyz.T).T
+
+
 
 
 
@@ -32,14 +35,33 @@ class TestRotoPoint(unittest.TestCase):
         self.rotopoint = RotoPoint()
 
     def test_rotate_points_rotates_one_point_abount_y_axis(self):
-        points = np.array([1, 1, 1], dtype='float16')
-        expected = np.array([1, 1, -1], dtype='float16')
+        points = np.array([[1, 1, 1]], dtype='float16')
+        expected = np.array([[1, 1, -1]], dtype='float16')
 
         result = self.rotopoint.rotate_points(points, np.pi / 2.0)
         print(result)
         print(expected)
-        self.assertTrue(np.allclose(expected, result,rtol=1e-03))
+        self.assertTrue(np.allclose(expected, result, rtol=1e-03))
 
+    def test_rotate_points_rotates_two_point_abount_y_axis(self):
+        points = np.array([[1, 1, 1], [0, 0, 0]], dtype='float16')
+        expected = np.array([[1, 1, -1], [0, 0, 0]], dtype='float16')
+
+        result = self.rotopoint.rotate_points(points, np.pi / 2.0)
+        print(result)
+        print(expected)
+        self.assertTrue(np.allclose(expected, result, rtol=1e-03))
+
+    def test_rotate_points_rotates_points_abount_y_axis_fast(self):
+        test_items = 1000
+        points = np.random.random((test_items, 3)).astype('float16')
+
+        start = time.time()
+        result = self.rotopoint.rotate_points(points, np.pi / 2.0)
+        total = time.time() - start
+
+        print("Time: {}ms or {}/ms".format(total * 1000, test_items / (total *1000)))
+        self.assertTrue(total * 1000 < 1.0, total)
 
 
 if __name__ == '__main__':
