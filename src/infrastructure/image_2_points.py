163caf4277cda33ec5bd02b10bdc7cfba53766f7
James Townley <software@peachyprinter.com>
2015-11-27 09:15:29 -0600

    Added simple and rotation cases

--- src/infrastructure/image_2_points.py
+++ src/infrastructure/image_2_points.py
@@ -11,13 +11,14 @@ class Image2Points(object):
     ):
         self._mm_per_pixel = self._mm_per_pixel(camera_pixels_shape_xy, camera_sensor_size_mm)
         self._laser_plane_normal = self._get_laser_plane_normal(center_intersection_xyz, laser_center_intersection_rad)
-        self._posisition_mask = self._get_laser_intersections_mask(camera_pixels_shape_xy, camera_focal_length_mm, self._mm_per_pixel, center_intersection_xyz, self._laser_plane_normal)
+        self._posisition_mask = self._get_laser_intersections_mask(camera_pixels_shape_xy, camera_focal_length_mm, self._mm_per_pixel, center_intersection_xyz, center_intersection_xyz, self._laser_plane_normal)
 
     def _get_laser_intersections_mask(
         self,
         camera_pixels_shape_xy,
         camera_focal_length_mm,
         mm_per_pixel,
+        center_intersection_xyz,
         laser_intersection_point,
         laser_plane_normal):
 
@@ -33,7 +34,7 @@ class Image2Points(object):
 
                     L2 = np.array([x_pos, y_pos, z_pos])
                     final[x_camera, y_camera] = (p_dot / np.dot(laser_plane_normal, L2)) * L2
-        return final
+        return final - center_intersection_xyz
 
     def _mm_per_pixel(self, camera_pixels_shape_xy, camera_sensor_size_mm):
         x_mm_per_pixel = camera_sensor_size_mm[0] / float(camera_pixels_shape_xy[0])
@@ -49,5 +50,21 @@ class Image2Points(object):
         v_31 = p_3 - p_1
         return np.cross(v_21, v_31)
 
+    def _rotation_matrix(self, theta):
+        rotation_axis = np.asarray([0, 1, 0], dtype='float16')
+        theta = np.asarray(theta, dtype='float16')
+        axis = rotation_axis/np.sqrt(np.dot(rotation_axis, rotation_axis))
+        a = np.cos(theta / 2)
+        b, c, d = -axis*np.sin(theta/2)
+        a2, b2, c2, d2 = a*a, b*b, c*c, d*d
+        bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
+        return np.array([[a2+b2-c2-d2, 2*(bc+ad), 2*(bd-ac)],
+                         [2*(bc-ad), a2+c2-b2-d2, 2*(cd+ab)],
+                         [2*(bd+ac), 2*(cd-ab), a2+d2-b2-c2]])
+
+    def _rotate_points(self, points_xyz, rotation_rad):
+        rotation_matrix = self._rotation_matrix(rotation_rad)
+        return np.dot(rotation_matrix, points_xyz.T).T
+
     def get_points(self, image, rotation_rad, roi):
-        return self._posisition_mask[image]
+        return self._rotate_points(self._posisition_mask[image], rotation_rad)
--- test/infrastructure_test/test_image_2_points.py
+++ test/infrastructure_test/test_image_2_points.py
@@ -17,34 +17,66 @@ from infrastructure.roi import ROI
 
 class PointConverterTest(unittest.TestCase):
 
-    def test_get_points_returns_expected_points_give_simple_camera_and_image(self):
-        camera_pixels_shape_xy = (3, 3)
-        i2p = Image2Points(
+    def setup_i2p(self, camera_pixels_shape_xy=(3, 3)):
+        return Image2Points(
             camera_focal_length_mm=9,
             camera_sensor_size_mm=(13.5, 13.5),
             camera_pixels_shape_xy=camera_pixels_shape_xy,
             laser_center_intersection_rad=np.pi / 4,
             center_intersection_xyz=(0, 0, -9),
             )
+
+    def assert_array(self, array1, array2, rtol=1e-05):
+        for idx in range(9):
+            self.assertTrue(np.allclose(array1[idx], array2[idx], rtol=rtol), "TEST:{}:  {} == {}".format(idx, array1[idx], array2[idx]))
+
+    def test_get_points_returns_expected_points_give_simple_camera_and_image(self):
+        camera_pixels_shape_xy = (3, 3)
+        i2p = self.setup_i2p(camera_pixels_shape_xy=camera_pixels_shape_xy)
         image = np.ones(camera_pixels_shape_xy).astype('bool')
         expected = np.array([
-            [-3.0, -3.0,  -6.0],  # -1, -1
-            [-3.0,  0.0,  -6.0],  # -1,  0 
-            [-3.0,  3.0,  -6.0],  # -1,  1 
-            [ 0,   -4.5,  -9.0],  #  0, -1
-            [ 0,    0.0,  -9.0],  #  0,  0 
-            [ 0,    4.5,  -9.0],  #  0,  1 
-            [ 9.0, -9.0, -18.0],  #  1, -1
-            [ 9.0,    0, -18.0],  #  1,  0 
-            [ 9.0,  9.0, -18.0],  #  1,  1 
+            [-3.0, -3.0,  3.0],  # -1, -1
+            [-3.0,  0.0,  3.0],  # -1,  0 
+            [-3.0,  3.0,  3.0],  # -1,  1 
+            [ 0,   -4.5,  0.0],  #  0, -1
+            [ 0,    0.0,  0.0],  #  0,  0 
+            [ 0,    4.5,  0.0],  #  0,  1 
+            [ 9.0, -9.0, -9.0],  #  1, -1
+            [ 9.0,    0, -9.0],  #  1,  0 
+            [ 9.0,  9.0, -9.0],  #  1,  1 
             ], dtype='float16')
-        
+
         result = i2p.get_points(image, 0, ROI(0, 0, 1, 1))
 
-        for idx in range(9):
-            print "TEST:{}:  {} == {}".format(idx, expected[idx], result[idx])
-            self.assertTrue(np.allclose(expected[idx], result[idx]))
+        self.assert_array(expected, result)
 
+
+    def test_get_points_should_rotate(self):
+        camera_pixels_shape_xy = (3, 3)
+        rotation = np.pi / 2.0
+        i2p = self.setup_i2p(camera_pixels_shape_xy=camera_pixels_shape_xy)
+        image = np.ones(camera_pixels_shape_xy).astype('bool')
+        expected = np.array([
+            [3.0, -3.0,   3.0],  # -1, -1
+            [3.0,  0.0,   3.0],  # -1,  0 
+            [3.0,  3.0,   3.0],  # -1,  1 
+            [ 0,   -4.5,  0.0],  #  0, -1
+            [ 0,    0.0,  0.0],  #  0,  0 
+            [ 0,    4.5,  0.0],  #  0,  1 
+            [-9.0, -9.0, -9.0],  #  1, -1
+            [-9.0,    0, -9.0],  #  1,  0 
+            [-9.0,  9.0, -9.0],  #  1,  1 
+            ], dtype='float16')
+
+        result = i2p.get_points(image, rotation, ROI(0, 0, 1, 1))
+
+        self.assert_array(expected, result, rtol=1e-03)
+    
+    def test_get_points_should_return_nothing_when_ray_and_plane_dont_intersect(self):
+        pass
+ 
+    def test_get_points_should_roi(self):
+        pass
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level='INFO')
