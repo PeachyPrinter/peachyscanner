c3d52d418d514338fa76ccbcbb0794c9cbf5b597
James Townley <software@peachyprinter.com>
2015-12-08 14:15:04 -0600

    adding to spike

--- spikes/spike_line.py
+++ spikes/spike_line.py
@@ -36,13 +36,27 @@ def sub(a, b):
         sub1[mask] = 0
         return sub1.astype('uint8')
 
+def show_line(a, line_number=100):
+    out = np.zeros((256, a.shape[1], 3), dtype='uint8')
+    line = a[line_number]
+    for pos in range(line.shape[0]):
+        val = line[pos]
+        color = (int(val), int(0), int(255 - val))
+        out = cv2.line(out, (pos, 255), (pos, 255 - val), color)
+        # out[255 - val, pos] = [val, 0, 255 - val]
+    return out
+
+
+
 source = cv2.imread('g.png')
 cv2.imshow('frame1', source)
 cv2.createTrackbar('min', 'frame1', 20, 255, new_var_a)
 cv2.createTrackbar('max', 'frame1', 126, 255, new_var_b)
 cv2.createTrackbar('cmode', 'frame1', 0, 3, new_var_b)
 cv2.createTrackbar('rmode', 'frame1', 0, 4, new_var_b)
+itera = 0
 while True:
+    itera = itera + 0.3
     img = source.copy()
     b, g, r = cv2.split( img)
     ab, ag, ar = np.average(b), np.average(g), np.average(r)
@@ -50,13 +64,10 @@ while True:
     g = sub(g, ag)
     r = sub(r, ar)
     rel = sub(r, g)
+    rel = (rel * (255.0 / np.max(rel))).astype('uint8')
     ret, thresh = cv2.threshold(rel, a_val, b_val, 0)
     im2, contours, hierarchy = cv2.findContours(thresh, r_mode[d_val], c_mode[c_val])
     thresh = (thresh > a_val).astype('uint8') * 255
-    print thresh.shape
-    print (type(thresh))
-    print (thresh.dtype)
-    cv2.imshow('frame3', thresh)
     notthresh = np.invert(thresh)
     img = cv2.bitwise_and(img, img, mask=notthresh)
     blue = np.zeros(img.shape, dtype='uint8')
@@ -64,9 +75,11 @@ while True:
     blues = cv2.bitwise_and(blue, blue, mask=thresh)
     img = img + blues
 
-
-    cv2.imshow('frame3', thresh)
-    cv2.imshow('frame2', blues)
+    pos = int(itera) % img.shape[0]
+    cv2.imshow('frame4', show_line(rel, pos))
+    cv2.line(rel, (0, pos), (rel.shape[1], pos), 255, 1)
+    # cv2.imshow('frame3', thresh)
+    cv2.imshow('frame2', rel)
     cv2.imshow('frame1', img)
     # cv2.imshow('frame2', curves)
     
