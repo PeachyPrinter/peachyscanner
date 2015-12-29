ace99e8bdf2dec77096e6185db82c0b343bd6a3b
James Townley <software@peachyprinter.com>
2015-12-29 10:42:05 -0600

    Another interation of the line finding spike with some tools for performance monitoring added

--- spikes/spike_line.py
+++ spikes/spike_line.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+import time
 
-a_val = int(30)
+a_val = int(200)
 b_val = int(255)
 c_val = int(0)
 d_val = int(0)
@@ -45,52 +46,66 @@ def show_line(a, line_number=100):
         out = cv2.line(out, (pos, 255), (pos, 255 - val), color)
     return out
 
-
+spf = [1]
 source = cv2.imread('g.png')
-cv2.imshow('frame1', source)
-cv2.createTrackbar('min', 'frame1', 30, 255, new_var_a)
-# cv2.createTrackbar('max', 'frame1', 126, 255, new_var_b)
-# cv2.createTrackbar('cmode', 'frame1', 0, 3, new_var_b)
-# cv2.createTrackbar('rmode', 'frame1', 0, 4, new_var_b)
+cv2.imshow('Master', source)
+cv2.createTrackbar('min', 'Master', 30, 255, new_var_a)
+# cv2.createTrackbar('max', 'Master', 126, 255, new_var_b)
+# cv2.createTrackbar('cmode', 'Master', 0, 3, new_var_b)
+# cv2.createTrackbar('rmode', 'Master', 0, 4, new_var_b)
 itera = 350
 dia_x = 7
 dia_y = 7
 _video_capture = cv2.VideoCapture(0)
 _video_capture.read()
 while True:
+    start_fps = time.time()
     # itera = itera + 0.3
     (retVal, source) = _video_capture.read()
     if retVal:
         img = source.copy()
-        b, g, r = cv2.split( img)
-        ab, ag, ar = np.average(b), np.average(g), np.average(r)
-        b = sub(b, ab)
-        g = sub(g, ag)
-        r = sub(r, ar)
-        rel = sub(r, g)
-        rel = (rel * (255.0 / np.max(rel))).astype('uint8')
 
-
+        start = time.time()
+        b, g, r = cv2.split(img)
+        # ab, ag, ar = np.average(b), np.average(g), np.average(r)
+        # b = sub(b, ab)
+        # g = sub(g, ag)
+        # r = sub(r, ar)
+        # rel = sub(r, g)
+        rel = (r * (255.0 / np.max(r))).astype('uint8')
         thresh = np.zeros(rel.shape, dtype='uint8')
-        thresh[rel > 30] = rel[rel > 30]
-        rel = thresh.copy()
+        thresh[rel > a_val] = rel[rel > a_val]
 
-        erosion = cv2.erode(rel, np.ones((2, 2), dtype=np.uint8), iterations=1)
+        # rel = thresh.copy()
+
+        erosion = cv2.erode(thresh, np.ones((2, 2), dtype=np.uint8), iterations=1)
         dilation = cv2.dilate(erosion, np.ones((3, 10), dtype=np.uint8), iterations=1)
-        dial = (rel == dilation).astype(np.uint8) * 255
-        dial = cv2.bitwise_and(dial, rel)
+        dial = (thresh == dilation).astype(np.uint8) * 255
+        dial = cv2.bitwise_and(dial, thresh)
 
+        total = (time.time() - start) * 1000
+
+        cv2.putText(img, "{: 5.2f} ms".format(total), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 0))
+        cv2.putText(img, "{: 5.2f} fps".format(float(len(spf)) / sum(spf)), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 0))
 
         pos = int(itera) % img.shape[0]
-        cv2.imshow('frame4', show_line(rel, pos))
-        cv2.line(rel, (0, pos), (rel.shape[1], pos), 255, 1)
-        cv2.imshow('di', dilation)
-        cv2.imshow('frame2', rel)
-        cv2.imshow('frame1', img)
 
-        cv2.imshow('dialate', cv2.resize(dial, (0, 0), fx=2.0, fy=2.0))
-        cv2.imshow('frame1', source)
-        cv2.createTrackbar('min', 'frame1', 30, 255, new_var_a)
+        master_image = np.zeros((960, 1280,3), dtype='uint8')
+        master_image[:480, 0:640, :] = img
+        master_image[480:, 0:640, 0] = rel
+        master_image[:480:, 640:, 1] = thresh
+        master_image[480:, 640:, 2] = dial
+
+
+        # cv2.imshow('frame4', show_line(rel, pos))
+        # cv2.line(rel, (0, pos), (rel.shape[1], pos), 255, 1)
+        # cv2.imshow('di', dilation)
+        # cv2.imshow('Relitve Red', rel)
+        # cv2.imshow('Threshold Red', thresh)
+        cv2.imshow('Master', master_image)
+        # cv2.imshow('Processed', dial)
+        # cv2.imshow('Master', source)
+        # cv2.createTrackbar('min', 'Master', 30, 255, new_var_a)
 
         key = cv2.waitKey(1) & 255
         if key == ord('q'):
@@ -111,5 +126,10 @@ while True:
         if key == ord('j'):
             dia_x = abs(dia_x + 1)
             print ("dialstion x,y: {},{}".format(dia_x, dia_y))
+
+        stop_fps = time.time() - start_fps
+        spf.append(stop_fps)
+        if len(spf) > 10:
+            spf = spf[-10:]
 
 cv2.destroyAllWindows()
